f6e2c01bc95fea11e9c675b962bb524507d22982
omar <omarsameh20017@gmail.com>
2024-09-30 04:17:16 +0300

    improve buttons

--- src/app/components/stepper/stepper.tsx
+++ src/app/components/stepper/stepper.tsx
@@ -201,6 +201,7 @@ const MyStepper = () => {
               borderRadius: "50%",
               border: "1px solid black",
               margin: "10px",
+              backgroundColor: "white",
             }}
             onClick={handleBack}
           >
