9ef9f201ecdf7ab38acf8e1eb278e4e4688a64e5
omar <omarsameh20017@gmail.com>
2024-09-30 04:09:01 +0300

    fix image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,11 @@ export default function Home() {
   return (
     <div>
       <Image
-        src=".\static\cover1.jpg"
+        src="/static/cover1.jpg"
         alt="cover letter"
         style={{ width: "100vw", height: "100vh", objectFit: "cover", zIndex: -999, position: "absolute", opacity: 0.2 }}
+        width={1920}
+        height={1080}
       />
       <MyStepper />
     </div>
